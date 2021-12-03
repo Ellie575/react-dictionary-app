ef84d37c023f094f12c6d4fd5df248038a7d9743
Ellie575 <elliekubo@hotmail.com>
2021-12-03 21:33:25 +1100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import React from "react";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary App</header>
+      <Dictionary />
+      <footer className="text-center">Coded by Ellie Kubo</footer>
     </div>
   );
 }
